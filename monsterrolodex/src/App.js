18c5743fcf560bdc632d18f6acd064e970a92042
Nikil <snikilpaul@gmail.com>
2025-02-08 12:05:23 +0530

    rewrote app.js to include name from state

--- monsterrolodex/src/App.js
+++ monsterrolodex/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
+class App extends Component {
+  
+  constructor(){
+
+    super();
+    this.state = {
+      name:"Nikil Paul",
+    }
+
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello {this.state.name} !</p>
     </div>
   );
-}
+}}
 
 export default App;
